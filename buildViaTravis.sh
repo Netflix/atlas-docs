894baf8918d550fc0c62c9a3369116feaaccd88a
Brian Harrington <brharrington@gmail.com>
2018-03-30 09:59:41 -0700

    add travis files
    Right now both PRs and master builds will just package
    it up. We'll need to follow up with credentials to support
    publishing to ghpages branch when merged to master. This
    is the first step for #2.

---
+++ buildViaTravis.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# This script will build the project.
+
+if [ "$TRAVIS_PULL_REQUEST" != "false" ] || [ "$BINTRAY_PUBLISH" == "false" ]; then
+  echo -e "Build Pull Request #$TRAVIS_PULL_REQUEST => Branch [$TRAVIS_BRANCH]"
+  make build
+elif [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_TAG" == "" ]; then
+  echo -e 'Build Branch with Snapshot => Branch ['$TRAVIS_BRANCH']'
+  make build # TODO: switch to release
+elif [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_TAG" != "" ]; then
+  echo -e 'Build Branch for Release => Branch ['$TRAVIS_BRANCH']  Tag ['$TRAVIS_TAG']'
+  make build # TODO: switch to release
+else
+  echo -e 'WARN: Should not be here => Branch ['$TRAVIS_BRANCH']  Tag ['$TRAVIS_TAG']  Pull Request ['$TRAVIS_PULL_REQUEST']'
+  make build
+fi
