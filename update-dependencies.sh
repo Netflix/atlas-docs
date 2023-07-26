1d737a1c897067dbb17f624d5c3e03ff45ec54c1
Matthew Johnson <mjjohnson33@gmail.com>
2023-07-25 20:43:22 -0400

    update python dependencies
    * Regenerate the whole stack of Python dependencies.
    * Add a script to automate the update process.
    * Add some development notes.

---
+++ update-dependencies.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+
+echo "-- remove current venv"
+rm -rf venv
+
+echo "-- remove current requirements.txt"
+rm requirements.txt
+
+echo "-- create new venv with no deps"
+./setup-venv.sh
+
+echo "-- activate venv"
+source venv/bin/activate
+
+
+DEPS=( \
+  mkdocs-material \
+  mkdocs-markdownextradata-plugin \
+  mkdocs-minify-plugin \
+  plugins/atlas-link-checker \
+  plugins/mkdocs-atlas-formatting-plugin \
+)
+
+echo "-- add dep tools to venv"
+pip3 install --upgrade pip wheel
+
+echo "-- install deps"
+pip3 install --upgrade ${DEPS[@]}
+
+echo "-- create requirements.txt"
+pip3 freeze |egrep -v "atlas-link-checker|mkdocs-atlas-formatting-plugin" > requirements.txt
+echo "-e plugins/atlas-link-checker" >> requirements.txt
+echo "-e plugins/mkdocs-atlas-formatting-plugin" >> requirements.txt
+
+echo "-- deactivate venv"
+deactivate
+
